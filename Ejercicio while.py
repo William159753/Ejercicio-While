1def619d6a533b6fe5e752fb83e617c54a075e92
William159753 <chonatawilliam@gmail.com>
2025-12-02 18:17:09 -0500

    First commit

---
+++ Ejercicio while.py
@@ -0,0 +1,6 @@
+contrasenia = input("Ingrese la contraseña: ")
+contra=str("1234a5")
+while contrasenia!= contra:
+    print("Contraseña incorrecta")
+    contrasenia = input ("Ingrese la contraseña: ")
+print("Contraseña correcta")
